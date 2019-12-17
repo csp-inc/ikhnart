45f161374e17a90db55be9d79cd5cdfc14463c6f
tonychang <tony@csp-inc.org>
2019-12-17 14:37:02 -0800

    updated image

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
-nvidia-docker run -it -p 8080:8080 -v $(pwd):/content -v /E:/E -w /content --rm tonychangcsp/keras:v1.1 jupyter notebook --port 8080 --ip 0.0.0.0 --no-browser --allow-root
+IMAGE_NAME=tonychangcsp/ikhnart:latest
+docker run -it \
+	-p 8080:8080 \
+	-v $(pwd):/content \
+	-w /content \
+	--rm \
+	$IMAGE_NAME \
+	jupyter notebook --port 8080 --ip 0.0.0.0 --no-browser --allow-root
 
